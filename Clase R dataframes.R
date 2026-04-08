6f9ecc6257756f83988ffccead09c92265c5b823
aalejandragv <alejandragvgc@gmail.com>
2026-04-08 11:11:29 +0200

    Add files via upload

--- Clase R dataframes.R
+++ Clase R dataframes.R
@@ -1,2 +1,27 @@
 df <- read.csv('https://raw.githubusercontent.com/asalber/manual-r/master/datos/colesterol.csv')
 df
+print(summary(df))
+head(df,5)
+tail(df,3)
+head(df,2)
+nombre  <- c("Carlos", "Maria", "Carmen", "Pedro", "Luis", "Sara")
+sexo    <- c("H", "M", "M", "H", "H", "M")
+fisica  <- c(6.7, 7.2, 5.5, NA, 3.5, 6.2)
+quimica <- c(8.1, 9.5, 5.0, 4.5, 5.0, 4.0)
+
+sexo <- factor(sexo)
+levels(sexo)  
+
+media <- (fisica + quimica) / 2
+media
+
+aprobado <- media >= 5
+aprobado
+
+nombre[aprobado]
+
+df <- data.frame(nombre, sexo, fisica, quimica)
+
+df$media <- media
+
+df[df$sexo == "M" & !is.na(df$media) & df$media >= 5, c("nombre", "media")]
